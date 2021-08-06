952ca641a21d303898e25aa8fdb6045f9844e287
Arshanaa <36689084+Arshanaa@users.noreply.github.com>
2021-08-06 09:57:41 +0800

    Update MyArray.java

--- GenericExercise/src/MyArray.java
+++ GenericExercise/src/MyArray.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Arshanaa
- */
 public class MyArray {
     public static void main(String []args){
         Integer[] numbers = {1,2,3,4,5};
